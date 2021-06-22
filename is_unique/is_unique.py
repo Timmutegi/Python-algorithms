f23e55148b3551c50613ef8605212ef3dfe5311c
Timmutegi <mutegi.timothy538@gmail.com>
2021-06-22 21:04:58 +0300

    Refactored solution into a function

--- is_unique/is_unique.py
+++ is_unique/is_unique.py
@@ -1,7 +1,11 @@
 # Implement an algorithm to determine if a string has all unique characters.
 
 # SOLUTION
+
+def isUnique(string):
+    string_set = set(string)
+    is_unique = True if len(string) == len(string_set) else False
+    print(is_unique)
+
 string = 'algorithm'
-string_set = set(string)
-is_unique = True if len(string) == len(string_set) else False
-print(is_unique)
+isUnique(string)
