69b3c085e1d7fa0312e34d11bee8c1429d330d69
Timmutegi <mutegi.timothy538@gmail.com>
2021-10-01 20:37:43 +0300

    Added two_sum solution

---
+++ two_sum/two_sum.py
@@ -0,0 +1,22 @@
+"""
+Given an array of integers, return indices of the two numbers 
+such that they add up to a specific target.
+"""
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        my_dict = {}
+        for index, num in enumerate(nums):
+            value = target - num
+            if value in my_dict and my_dict[value] != index:
+                return [my_dict[value], index]
+
+            my_dict[nums[index]] = index
+                
+nums = [2, 4, 5, 6]
+target = 11
+print(Solution().twoSum(nums, target))
