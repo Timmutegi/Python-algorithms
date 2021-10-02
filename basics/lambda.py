33ed8c335f81da136a6e76ac20bf6bf982d69e49
Timmutegi <mutegi.timothy538@gmail.com>
2021-10-02 09:49:17 +0300

    Added lambda function example

---
+++ basics/lambda.py
@@ -0,0 +1,9 @@
+# x = lambda a : a + 10
+# print(x(5))
+# line = [(10, True), (20, False), (15, True), (30, False), (5, True), (10, False)]
+# line.sort(key=lambda x: (x[0], -x[1])) 
+# print(line)
+# [(5, True), (10, True), (10, False), (15, True), (20, False), (30, False)]
+
+x = lambda a, b : a * b
+print(x(5, 6))
