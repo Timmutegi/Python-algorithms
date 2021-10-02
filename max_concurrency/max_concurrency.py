1858f79c94eedd58a45ae2d1ac2b43546240dc78
Timmutegi <mutegi.timothy538@gmail.com>
2021-10-02 09:48:19 +0300

    Added max concurrency solution

---
+++ max_concurrency/max_concurrency.py
@@ -0,0 +1,36 @@
+"""
+Given a list of start time and end times of a list of queries, how will you find the maximum
+concurrency shown by the system?
+"""
+
+def max_concurrency(times):
+    """
+    :type times: List[int]
+    """
+    line = []
+    
+    for time in times:
+        line.append((time[0], True))
+        line.append((time[1], False))
+    
+    # [(10, True), (20, False), (15, True), (30, False), (5, True), (10, False)]
+
+    line.sort(key=lambda x: (x[0], -x[1]))
+    # [(5, True), (10, True), (10, False), (15, True), (20, False), (30, False)]
+    history_max = 0
+    count = 0
+
+    for _, is_start in line:
+        if is_start:
+            count += 1
+            history_max = max(history_max, count)
+        else:
+            count -= 1
+
+    return history_max 
+
+    # print(line)
+
+input1 = [(10, 20), (15, 30), (5, 10)] ## [(5, True), (10, True), (10, False), (15, True), (20, False), (30, False)]
+
+print(max_concurrency(input1))
