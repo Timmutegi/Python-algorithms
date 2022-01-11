d13c35163000b11832711aec6d1a1e707fe6ca14
Timmutegi <mutegi.timothy538@gmail.com>
2022-01-11 18:29:48 +0200

    Added valid palindrome solution 2

---
+++ valid_palindrome/valid_palindrome_v2.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        left, right = 0, len(s) - 1
+        
+        while left < right:
+            while left < right and not self.alphaNum(s[left]):
+                left += 1
+            while right > left and not self.alphaNum(s[right]):
+                right -= 1
+                
+            if s[left].lower() != s[right].lower():
+                return False
+            
+            left, right = left + 1, right - 1
+            
+            
+        return True
+    
+    def alphaNum(self, c):
+        return (ord('A') <= ord(c) <= ord('Z') or 
+                ord('a') <= ord(c) <= ord('z') or
+                ord('0') <= ord(c) <= ord('9'))    
+        
+print(Solution().isPalindrome('A man, a plan, a canal: Panama'))
