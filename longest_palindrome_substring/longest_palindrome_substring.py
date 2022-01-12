3e97172d98f4d7422ce21ec6f637a882bcfc93ca
Timmutegi <mutegi.timothy538@gmail.com>
2022-01-12 21:10:01 +0200

    Added longest palindrome solution

---
+++ longest_palindrome_substring/longest_palindrome_substring.py
@@ -0,0 +1,27 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        res = ""
+        resLen = 0
+        
+        for i in range(len(s)):
+            # Odd Length
+            left, right = i, i
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                if (right - left + 1) > resLen:
+                    res = s[left:right+1]
+                    resLen = right - left + 1
+                left -= 1
+                right += 1
+                
+            # Even Length
+            left, right = i, i + 1
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                if (right - left + 1) > resLen:
+                    res = s[left:right+1]
+                    resLen = right - left + 1
+                left -= 1
+                right += 1   
+                
+        return res
+    
+print(Solution().longestPalindrome('babad'))
