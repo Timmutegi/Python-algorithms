26dfff010a0287a1f8e414436bb986bdff2f122d
Timmutegi <mutegi.timothy538@gmail.com>
2022-01-21 11:51:38 +0200

    Added two_sum solution

--- two_sum/two_sum.py
+++ two_sum/two_sum.py
@@ -4,11 +4,6 @@ such that they add up to a specific target.
 """
 class Solution(object):
     def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
         my_dict = {}
         for index, num in enumerate(nums):
             value = target - num
