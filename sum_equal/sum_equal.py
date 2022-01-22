df1fe4c3f925ff3b90a9db2c11f0545a0f164605
Timmutegi <mutegi.timothy538@gmail.com>
2022-01-22 20:19:05 +0200

    Added sum equal solution

--- sum_equal/sum_equal.py
+++ sum_equal/sum_equal.py
@@ -3,12 +3,12 @@ Given an array of integers and a value, determine if there are any two
 integers in the array whose sum is equal to the given value. 
 Return true if the sum exists and return false if it does not.
 """
-def find_sum_of_two(A, val):
+def find_sum_of_two(arr, val):
     found_values = set()
-    for a in A:
-        if val - a in found_values:
+    for num in arr:
+        if val - num in found_values:
             return True
 
-        found_values.add(a)
+        found_values.add(num)
 
     return False
