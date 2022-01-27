2fdc27f418774762c463926bee84205ab5be7383
Timmutegi <mutegi.timothy538@gmail.com>
2022-01-27 20:34:18 +0200

    Added fibonacci series solution

---
+++ fibonacci_series/fibonacci.py
@@ -0,0 +1,12 @@
+class Solution:
+    def fibonacci(self, n):
+        if n == 0:
+            return 0
+        
+        if n == 1 or n == 2:
+            return 1
+        
+        return self.fibonacci(n-1) + self.fibonacci(n - 2)
+    
+    
+print(Solution().fibonacci(9))
