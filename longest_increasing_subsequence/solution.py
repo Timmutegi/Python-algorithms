8ce03573ce672bb48560d5f47520552dd75c7a7a
Timmutegi <mutegi.timothy538@gmail.com>
2022-02-01 21:22:38 +0200

    Added longest increasing subsequence solution

---
+++ longest_increasing_subsequence/solution.py
@@ -0,0 +1,9 @@
+"""
+Find the length of the longest increasing subsequence of an array
+"""
+class Solution:
+    def longest_increasing_subsequence(self, arr):
+        print(arr)        
+                
+        
+print(Solution().longest_increasing_subsequence([1,5,2,3,8,10,6,3]))
