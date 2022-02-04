882f4214a6f1c75db6c41fb88f2e56eab5c9a0e8
Timmutegi <mutegi.timothy538@gmail.com>
2022-02-04 19:53:12 +0200

    Added longest increasing subsequence solution

--- longest_increasing_subsequence/solution.py
+++ longest_increasing_subsequence/solution.py
@@ -2,8 +2,32 @@
 Find the length of the longest increasing subsequence of an array
 """
 class Solution:
-    def longest_increasing_subsequence(self, arr):
-        print(arr)        
-                
+    def lis(arr):
+        n = len(arr)
+    
+        # Declare the list (array) for LIS and
+        # initialize LIS values for all indexes
+        lis = [1]*n
+    
+        # Compute optimized LIS values in bottom up manner
+        for i in range(1, n):
+            for j in range(0, i):
+                if arr[i] > arr[j] and lis[i] < lis[j] + 1:
+                    lis[i] = lis[j]+1
+    
+        # Initialize maximum to 0 to get
+        # the maximum of all LIS
+        maximum = 0
+    
+        # Pick maximum of all LIS values
+        for i in range(n):
+            maximum = max(maximum, lis[i])
+    
+        return maximum
+    # end of lis function
+    
+    
+# Driver program to test above function
+arr = [10, 22, 9, 33, 21, 50, 41, 60]               
         
-print(Solution().longest_increasing_subsequence([1,5,2,3,8,10,6,3]))
+print(Solution().longest_increasing_subsequence(arr))
