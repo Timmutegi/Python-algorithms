048ae78a614ffdbef51f762c5cfdc3947716b732
Timmutegi <mutegi.timothy538@gmail.com>
2022-02-21 19:44:03 +0200

    Removed full-stop

--- lonely_number/lonely_number.py
+++ lonely_number/lonely_number.py
@@ -1,7 +1,7 @@
 """
 In a given array of numbers, one element will show up once and the others will each show
 up twice. Can you find the number that only appears once in O(n) linear time? 
-Bonus points if you can do it in O(1) space as well.
+Bonus points if you can do it in O(1) space as well
 """
 class Solution:
     def lonelyNumber(self, numbers):
