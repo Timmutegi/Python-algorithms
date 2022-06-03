923213849f279e8c3e0334da1e29aeacbf8c12e7
Timmutegi <mutegi.timothy538@gmail.com>
2022-06-03 17:02:40 -0600

    Updated the documentation

--- PythonAdvanced/Generators/generators.py
+++ PythonAdvanced/Generators/generators.py
@@ -2,6 +2,9 @@
 Generator functions allow you to declare a function that behaves like an iterator.
 
 They allow programmers to make an iterator in a fast, easy, and clean way.
+
+They have lazy execution (producing items only when asked for). 
+For this reason, a generator expression is much more memory efficient than an equivalent list comprehension.
 """
 def check_prime(number):    
     for divisor in range(2, int(number ** 0.5) + 1):        
