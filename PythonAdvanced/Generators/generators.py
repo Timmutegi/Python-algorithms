bbde1b96e8cb680fb2335d1722bc359aa327587e
Timmutegi <mutegi.timothy538@gmail.com>
2022-06-03 15:11:22 -0600

    Added generators code examples

---
+++ PythonAdvanced/Generators/generators.py
@@ -0,0 +1,21 @@
+"""
+Generator functions allow you to declare a function that behaves like an iterator.
+
+They allow programmers to make an iterator in a fast, easy, and clean way.
+"""
+def check_prime(number):    
+    for divisor in range(2, int(number ** 0.5) + 1):        
+        if number % divisor == 0:            
+            return False    
+        return True
+
+def Primes(max):    
+    number = 1    
+    while number < max:        
+        number += 1        
+        if check_prime(number):            
+            yield number
+
+primes = Primes(1000)
+for prime in primes:
+    print(prime)
