1eee2434ebc076f10ac1e75312149c9516627703
Timmutegi <mutegi.timothy538@gmail.com>
2022-06-08 15:38:02 -0600

    updated documentation

--- LitCode/valid_anagram/valid_anagram_v2.py
+++ LitCode/valid_anagram/valid_anagram_v2.py
@@ -4,6 +4,9 @@ Given two strings s and t, return true if t is an anagram of s, and false otherw
 An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, 
 typically using all the original letters exactly once.
 """
+from collections import Counter
+
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
--- LitCode/valid_anagram/valid_anagram_v3.py
+++ LitCode/valid_anagram/valid_anagram_v3.py
@@ -5,7 +5,7 @@ An Anagram is a word or phrase formed by rearranging the letters of a different 
 typically using all the original letters exactly once.
 
 Time Complexity: O(S+T)
-Space Complexity: 
+Space Complexity: O(S+T)
 """
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
