5ef896dd5c7bc6770cd28d207f401274a241d46a
Timmutegi <mutegi.timothy538@gmail.com>
2022-07-11 23:09:45 -0600

    Removed typo

--- GrokkingCodingInterview/SlidingWindow/fruits_into_basket.py
+++ GrokkingCodingInterview/SlidingWindow/fruits_into_basket.py
@@ -5,7 +5,7 @@ You will be given two baskets, and your goal is to pick as many fruits as possib
 You will be given an array of characters where each character represents a fruit tree. The farm has following restrictions:
 
 Each basket can have only one type of fruit. There is no limit to how many fruit a basket can hold.
-You can start with any tree, but you can’t skip a tree once you have started.
+You can start with any tree, but you can't skip a tree once you have started.
 You will pick exactly one fruit from every tree until you cannot, i.e., you will stop when you have to pick from a third fruit type.
 """
 def fruits_into_baskets(fruits):
@@ -28,5 +28,3 @@ def fruits_into_baskets(fruits):
 
     max_length = max(max_length, window_end - window_start + 1)
   return max_length
-
-  return -1
