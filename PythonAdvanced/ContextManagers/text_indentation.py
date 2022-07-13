811ba3ca410e0f1ba5ec6c6d940d6e0457f42889
Timmutegi <mutegi.timothy538@gmail.com>
2022-07-12 22:35:14 -0600

    Indentation API example

---
+++ PythonAdvanced/ContextManagers/text_indentation.py
@@ -0,0 +1,25 @@
+class Indenter:
+    def __init__(self):
+        self.level = -1
+
+    def __enter__(self):
+        self.level += 1
+        return self
+
+    def __exit__(self, exc_type, exc_value, exc_tb):
+        self.level -= 1
+
+    def print(self, text):
+        print(" " * self.level + text)
+
+def main():
+    with Indenter() as indent:
+        indent.print("hi")
+        with indent:
+            indent.print("hello")
+            with indent:
+                indent.print("bonjour")
+
+        indent.print("hey")
+
+main()
