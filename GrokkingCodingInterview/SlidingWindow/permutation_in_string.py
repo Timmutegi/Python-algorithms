fe4152c17f6c2830666cac89a8a7ee29c7c924d1
Timmutegi <mutegi.timothy538@gmail.com>
2022-07-25 21:59:09 -0600

    Added permutation in string solution

---
+++ GrokkingCodingInterview/SlidingWindow/permutation_in_string.py
@@ -0,0 +1,39 @@
+def find_permutation(str1, pattern):
+    window_start, matched = 0, 0
+    char_frequency = {}
+
+    for chr in pattern:
+        if chr not in char_frequency:
+            char_frequency[chr] = 0
+        char_frequency[chr] += 1
+
+    # Match all the characters from the 'char_frequency' with the current window
+    # Try to extend the range [window_start, window_end]
+    for window_end in range(len(str1)):
+        right_char = str1[window_end]
+        if right_char in char_frequency:
+            char_frequency[right_char] -= 1
+            if char_frequency[right_char] == 0:
+                matched += 1
+
+
+        if matched == len(char_frequency):
+            return True
+
+        # Shrink the window by one charater
+        if window_end >= len(pattern) - 1:
+            left_char = str1[window_start]
+            window_start += 1
+            if left_char in char_frequency:
+                if char_frequency[left_char] == 0:
+                    matched -= 1
+                char_frequency[left_char] += 1
+
+    return False
+
+
+def main():
+    print('Permutation exist: ' + str(find_permutation("oidbcaf", "abc")))
+
+
+main()
