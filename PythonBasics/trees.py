999cfee5f0bc770effa8cadc3eacf6e434b4e1ef
Timmutegi <mutegi.timothy538@gmail.com>
2022-08-30 13:00:04 -0600

    Added tree traversal

--- PythonBasics/trees.py
+++ PythonBasics/trees.py
@@ -17,6 +17,43 @@ class Node:
 
         print("Value is not in tree")
 
+    """
+    Visit the nodes starting from the root then going to the left as much as you can
+    """
+    def traversePreorder(self):
+        print(self.data)
+
+        if self.left:
+            self.left.traversePreorder()
+
+        if self.right:
+            self.right.traversePreorder()
+    
+    """
+    Start with the smallest node then go left to right
+    Start with the smallest node then go to the largest node in order
+    """
+    def traverseInorder(self):
+        if self.left:
+            self.left.traverseInorder()
+
+        print(self.data)
+
+        if self.right:
+            self.right.traverseInorder()
+    
+    """
+    Start with the left most node to the right visiting the root last
+    """
+    def traversePostorder(self):
+        if self.left:
+            self.left.traversePostorder()
+
+        if self.right:
+            self.right.traversePostorder()
+
+        print(self.data)
+
 class Tree:
     def __init__(self, root, name=''):
         self.root = root
@@ -24,6 +61,16 @@ class Tree:
 
     def search(self, target):
         return self.root.search(target)
+
+    def traversePreorder(self):
+        self.root.traversePreorder()
+
+    def traverseinorder(self):
+        self.root.traverseInorder()
+
+    def traversePostorder(self):
+        self.root.traversePostorder()
+
 
 node = Node(10)
 
@@ -39,9 +86,18 @@ node.right.right = Node(100)
 myTree = Tree(node, 'Tim\'s Tree')
 
 print(myTree.name)
-# print(myTree.root.left.data)
+print(myTree.root.left.data)
 
-# print(myTree.root.right.right.data)
+print(myTree.root.right.right.data)
 
 found = myTree.search(100)
 print(found.data)
+
+print("Preorder Traversal")
+myTree.traversePreorder()
+
+print("Inorder Traversal")
+myTree.traverseinorder()
+
+print("Postorder Traversal")
+myTree.traversePostorder()
