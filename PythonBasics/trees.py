0bee911584a1fdd8c10b4a440b36128113b26d04
Timmutegi <mutegi.timothy538@gmail.com>
2022-08-30 12:41:17 -0600

    Added tree data structure

---
+++ PythonBasics/trees.py
@@ -0,0 +1,47 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def search(self, target):
+        if self.data == target:
+            print("Found it!")
+            return self
+
+        if self.left and self.data > target:
+            return self.left.search(target)
+
+        if self.right and self.data < target:
+            return self.right.search(target)
+
+        print("Value is not in tree")
+
+class Tree:
+    def __init__(self, root, name=''):
+        self.root = root
+        self.name = name
+
+    def search(self, target):
+        return self.root.search(target)
+
+node = Node(10)
+
+node.left = Node(5)
+node.right = Node(15)
+
+node.left.left = Node(2)
+node.left.right = Node(6)
+
+node.right.left = Node(13)
+node.right.right = Node(100)
+
+myTree = Tree(node, 'Tim\'s Tree')
+
+print(myTree.name)
+# print(myTree.root.left.data)
+
+# print(myTree.root.right.right.data)
+
+found = myTree.search(100)
+print(found.data)
